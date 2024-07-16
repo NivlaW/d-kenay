1eb4cceddd070689a2599ccb56518f4ef7d1bd4f
Fattahahmad <fattah.ahmad04@gmail.com>
2024-07-16 20:06:25 +0700

    Mindahin file ke folder view + nambah asset

--- view/tabel_menu.php
+++ view/tabel_menu.php
@@ -1,22 +1,20 @@
-          <div class="content-wrapper">
+<div class="content-wrapper">
             <div class="row">      
             <div class="col-lg-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
-                  <h4 class="card-title">Tabel Pesanan</h4>
-                  <button type="button" class="btn btn-outline-primary btn-fw"> <i class="fa fa-plus-square-o"></i> Tambah Data</button>
+                  <h4 class="card-title">Tabel Menu</h4>
+                  <a href="" class="btn btn-outline-primary btn-fw"> <i class="fa fa-plus-square-o"></i> Tambah Data</a>
+                  <a href="" class="btn btn-outline-danger btn-fw"> <i class="fa fa-trash-o"></i> Hapus Data</a>
                   <div class="table-responsive">
                     <table class="table table-hover">
                       <thead>
                         <tr>
                           <th></th>
                           <th>No.</th>
-                          <th>Tanggal Pesanan</th>
-                          <th>Nama Pemesan</th>
-                          <th>Alamat Pemesan</th>
-                          <th>Makanan</th>
-                          <th>Jumlah Pesanan</th>
-                          <th>Total</th>
+                          <th>Nama Menu</th>
+                          <th>Harga</th>
+                          <th>Foto</th>
                         </tr>
                       </thead>
                       <tbody>
