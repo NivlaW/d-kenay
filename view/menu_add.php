a50f42bab88e099081da385a27fe09618fb0de9c
rifqiaql <rifqiaqila@gmail.com>
2024-07-19 23:10:53 +0700

    fix menu_add

--- view/menu_add.php
+++ view/menu_add.php
@@ -12,7 +12,7 @@
                 <input type="text" class="form-control" id="" name="harga_makanan" placeholder="">
             </div>
             <div class="form-group">
-                <label for="">Foto</label>
+                <label for="">Foto Menu</label>
                 <input type="file" class="form-control" id="" name="foto" placeholder="">
             </div>
             <button type="submit" class="btn btn-primary me-2">Submit</button>
@@ -21,24 +21,30 @@
     </div>
 </div>
 
-
 <?php
-if($_SERVER['REQUEST_METHOD'] == "POST"){
+if($_SERVER['REQUEST_METHOD']=="POST"){
     include "../koneksi.php";
 
-    $nama = $_FILES['foto']['name'];
+    $name = $_FILES['foto']['name'];
     $file_tmp = $_FILES['foto']['tmp_name'];
-    $n_random = rand(1, 9999);
-    $nama_baru = $n_random . '-' . $nama;
-    $folder = "foto/";
+    $n_random = rand(1,9999);
+    $nama_baru = $n_random . '-' . $name;
+    $folder = "foto_/";
 
     move_uploaded_file($file_tmp, $folder . $nama_baru);
 
-    $query = mysqli_query($con, "insert into tbl_menu (nama_menu, harga_makanan, foto) values ('$_POST[nama_menu]', '$_POST[harga_makanan]', '$nama_baru'");
+    $query = mysqli_query($con, "INSERT INTO tbl_menu (nama_menu, harga_makanan, foto) VALUES ('$_POST[nama_menu]', '$_POST[harga_makanan]', '$nama_baru')");
 
-    echo "<script>
-            alert('Data Berhasil Disimpan');
-            document.location='index.php?page=menu';
-        </script>";
-};
+    if ($query) {
+        echo "<script language='JavaScript'>
+            alert('Data Berhasil disimpan');
+            document.location= 'index.php?page=menu';
+          </script>";
+    } else {
+        echo "<script language='JavaScript'>
+            alert('Gagal menyimpan data');
+            document.location= 'index.php?page=menu_add';
+          </script>";
+    }
+}
 ?>
