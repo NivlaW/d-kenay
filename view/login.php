4877abd1ac2798ed546cfcd5805008df27ea9ff3
Fattahahmad <fattah.ahmad04@gmail.com>
2024-07-19 21:00:47 +0700

    benerin tampilan login + bisa login

--- view/login.php
+++ view/login.php
@@ -1,6 +1,6 @@
  <?php
 if($_SERVER['REQUEST_METHOD']=="POST"){
-    include "koneksi.php";
+    include "../koneksi.php";
     $user = $_POST['username'];
     $pass = $_POST['password'];
     $sql = mysqli_query($con, "select * from tbl_user where username = '$user' AND password='$pass'");
@@ -30,7 +30,6 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    Required meta tags 
     <meta charset="utf-8" />
     <meta
       name="viewport"
@@ -38,39 +37,39 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     />
     <title>Star Admin2</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="../../assets/vendors/feather/feather.css" />
+    <link rel="stylesheet" href="../assets/vendors/feather/feather.css" />
     <link
       rel="stylesheet"
-      href="../../assets/vendors/mdi/css/materialdesignicons.min.css"
+      href="../assets/vendors/mdi/css/materialdesignicons.min.css"
     />
     <link
       rel="stylesheet"
-      href="../../assets/vendors/ti-icons/css/themify-icons.css"
+      href="../assets/vendors/ti-icons/css/themify-icons.css"
     />
     <link
       rel="stylesheet"
-      href="../../assets/vendors/font-awesome/css/font-awesome.min.css"
+      href="../assets/vendors/font-awesome/css/font-awesome.min.css"
     />
-    <link rel="stylesheet" href="../../assets/vendors/typicons/typicons.css" />
+    <link rel="stylesheet" href="../assets/vendors/typicons/typicons.css" />
     <link
       rel="stylesheet"
-      href="../../assets/vendors/simple-line-icons/css/simple-line-icons.css"
-    />
-    <link
-      rel="stylesheet"
-      href="../../assets/vendors/css/vendor.bundle.base.css"
+      href="../assets/vendors/simple-line-icons/css/simple-line-icons.css"
     />
     <link
       rel="stylesheet"
-      href="../../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css"
+      href="../assets/vendors/css/vendor.bundle.base.css"
+    />
+    <link
+      rel="stylesheet"
+      href="../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css"
     />
     <!-- endinject -->
     <!-- Plugin css for this page -->
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="../../assets/css/style.css" />
+    <link rel="stylesheet" href="../assets/css/style.css" />
     <!-- endinject -->
-    <link rel="shortcut icon" href="../../assets/images/favicon.png" />
+    <link rel="shortcut icon" href="../assets/images/favicon.png" />
   </head>
   <body>
     <div class="container-scroller">
@@ -80,14 +79,15 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
             <div class="col-lg-4 mx-auto">
               <div class="auth-form-light text-left py-5 px-4 px-sm-5">
                 <div class="brand-logo">
-                  <img src="../../assets/images/logo 1.svg" alt="logo" />
+                  <img src="../assets/images/logo 1.svg" alt="logo" />
                 </div>
                 <h4>Login Admin</h4>
                 <h6 class="fw-light">Login Dashboard.</h6>
-                <form class="pt-3">
+                <form method="POST" class="pt-3">
                   <div class="form-group">
                     <input
-                      type="email"
+                      type="text"
+                      name="username"
                       class="form-control form-control-lg"
                       id="exampleInputEmail1"
                       placeholder="Username"
@@ -96,17 +96,14 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
                   <div class="form-group">
                     <input
                       type="password"
+                      name="password"
                       class="form-control form-control-lg"
                       id="exampleInputPassword1"
                       placeholder="Password"
                     />
                   </div>
                   <div class="mt-3 d-grid gap-2">
-                    <a
-                      class="btn btn-block btn-primary btn-lg fw-medium auth-form-btn"
-                      href="../../index.html"
-                      >SIGN IN</a
-                    >
+                    <input type="submit" class="btn btn-block btn-primary btn-lg fw-medium auth-form-btn" name="submit" class="btn btn-primary" value="login">
                   </div>
                 </form>
               </div>
@@ -119,17 +116,17 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
-    <script src="../../assets/vendors/js/vendor.bundle.base.js"></script>
-    <script src="../../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+    <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+    <script src="../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
     <!-- End plugin js for this page -->
     <!-- inject:js -->
-    <script src="../../assets/js/off-canvas.js"></script>
-    <script src="../../assets/js/template.js"></script>
-    <script src="../../assets/js/settings.js"></script>
-    <script src="../../assets/js/hoverable-collapse.js"></script>
-    <script src="../../assets/js/todolist.js"></script>
+    <script src="../assets/js/off-canvas.js"></script>
+    <script src="../assets/js/template.js"></script>
+    <script src="../assets/js/settings.js"></script>
+    <script src="../assets/js/hoverable-collapse.js"></script>
+    <script src="../assets/js/todolist.js"></script>
     <!-- endinject -->
   </body>
 </html>
