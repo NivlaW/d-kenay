0ab2641e114a4008f86aa705eea031fe2c66c558
NivlaW <1nivla1alvin@gmail.com>
2024-07-22 17:01:39 +0700

    deleted file and update index admin

--- view/index.php
+++ view/index.php
@@ -50,8 +50,7 @@
         <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex align-items-top flex-row">
             <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
                 <div class="me-3">
-                    <button class="navbar-toggler navbar-toggler align-self-center" type="button"
-                        data-bs-toggle="minimize">
+                    <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-bs-toggle="minimize">
                         <span class="icon-menu"></span>
                     </button>
                 </div>
@@ -73,151 +72,24 @@
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <form class="search-form" action="#">
-                            <i class="icon-search"></i>
-                            <input type="search" class="form-control" placeholder="Search Here" title="Search here" />
-                        </form>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link count-indicator" id="notificationDropdown" href="#"
-                            data-bs-toggle="dropdown">
-                            <i class="icon-bell"></i>
-                            <span class="count"></span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list pb-0"
-                            aria-labelledby="notificationDropdown">
-                            <a class="dropdown-item py-3 border-bottom">
-                                <p class="mb-0 fw-medium float-start">
-                                    You have 4 new notifications
-                                </p>
-                                <span class="badge badge-pill badge-primary float-end">View all</span>
-                            </a>
-                            <a class="dropdown-item preview-item py-3">
-                                <div class="preview-thumbnail">
-                                    <i class="mdi mdi-alert m-auto text-primary"></i>
-                                </div>
-                                <div class="preview-item-content">
-                                    <h6 class="preview-subject fw-normal text-dark mb-1">
-                                        Application Error
-                                    </h6>
-                                    <p class="fw-light small-text mb-0">Just now</p>
-                                </div>
-                            </a>
-                            <a class="dropdown-item preview-item py-3">
-                                <div class="preview-thumbnail">
-                                    <i class="mdi mdi-lock-outline m-auto text-primary"></i>
-                                </div>
-                                <div class="preview-item-content">
-                                    <h6 class="preview-subject fw-normal text-dark mb-1">
-                                        Settings
-                                    </h6>
-                                    <p class="fw-light small-text mb-0">Private message</p>
-                                </div>
-                            </a>
-                            <a class="dropdown-item preview-item py-3">
-                                <div class="preview-thumbnail">
-                                    <i class="mdi mdi-airballoon m-auto text-primary"></i>
-                                </div>
-                                <div class="preview-item-content">
-                                    <h6 class="preview-subject fw-normal text-dark mb-1">
-                                        New user registration
-                                    </h6>
-                                    <p class="fw-light small-text mb-0">2 days ago</p>
-                                </div>
-                            </a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link count-indicator" id="countDropdown" href="#" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            <i class="icon-mail icon-lg"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list pb-0"
-                            aria-labelledby="countDropdown">
-                            <a class="dropdown-item py-3">
-                                <p class="mb-0 fw-medium float-start">
-                                    You have 7 unread mails
-                                </p>
-                                <span class="badge badge-pill badge-primary float-end">View all</span>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <img src="../assets/images/faces/face10.jpg" alt="image"
-                                        class="img-sm profile-pic" />
-                                </div>
-                                <div class="preview-item-content flex-grow py-2">
-                                    <p class="preview-subject ellipsis fw-medium text-dark">
-                                        Marian Garner
-                                    </p>
-                                    <p class="fw-light small-text mb-0">
-                                        The meeting is cancelled
-                                    </p>
-                                </div>
-                            </a>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <img src="../assets/images/faces/face12.jpg" alt="image"
-                                        class="img-sm profile-pic" />
-                                </div>
-                                <div class="preview-item-content flex-grow py-2">
-                                    <p class="preview-subject ellipsis fw-medium text-dark">
-                                        David Grey
-                                    </p>
-                                    <p class="fw-light small-text mb-0">
-                                        The meeting is cancelled
-                                    </p>
-                                </div>
-                            </a>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <img src="../assets/images/faces/face1.jpg" alt="image"
-                                        class="img-sm profile-pic" />
-                                </div>
-                                <div class="preview-item-content flex-grow py-2">
-                                    <p class="preview-subject ellipsis fw-medium text-dark">
-                                        Travis Jenkins
-                                    </p>
-                                    <p class="fw-light small-text mb-0">
-                                        The meeting is cancelled
-                                    </p>
-                                </div>
-                            </a>
-                        </div>
-                    </li>
+
                     <li class="nav-item dropdown d-none d-lg-block user-dropdown">
                         <a class="nav-link" id="UserDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img class="img-xs rounded-circle" src="../assets/images/faces/face8.jpg"
-                                alt="Profile image" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#000000" viewBox="0 0 256 256">
+                                <path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z">
+                                </path>
+                            </svg>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
                             <div class="dropdown-header text-center">
-                                <img class="img-md rounded-circle" src="../assets/images/faces/face8.jpg"
-                                    alt="Profile image" />
-                                <p class="mb-1 mt-3 fw-semibold">Allen Moreno</p>
-                                <p class="fw-light text-muted mb-0">allenmoreno@gmail.com</p>
+                                <p class="mb-1 mt-3 fw-semibold">Fatah</p>
+                                <p class="fw-light text-muted mb-0">Keuangan</p>
                             </div>
-                            <a class="dropdown-item"><i
-                                    class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i>
-                                My Profile
-                                <span class="badge badge-pill badge-danger">1</span></a>
-                            <a class="dropdown-item"><i
-                                    class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i>
-                                Messages</a>
-                            <a class="dropdown-item"><i
-                                    class="dropdown-item-icon mdi mdi-calendar-check-outline text-primary me-2"></i>
-                                Activity</a>
-                            <a class="dropdown-item"><i
-                                    class="dropdown-item-icon mdi mdi-help-circle-outline text-primary me-2"></i>
-                                FAQ</a>
-                            <a class="dropdown-item"><i
-                                    class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign Out</a>
+                            <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign Out</a>
                         </div>
                     </li>
                 </ul>
-                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
-                    data-bs-toggle="offcanvas">
+                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-bs-toggle="offcanvas">
                     <span class="mdi mdi-menu"></span>
                 </button>
             </div>
@@ -247,8 +119,7 @@
                     </li>
                     <li class="nav-item nav-category">UI Elements</li>
                     <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
-                            aria-controls="ui-basic">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
                             <i class="menu-icon mdi mdi-floor-plan"></i>
                             <span class="menu-title">UI Elements</span>
                             <i class="menu-arrow"></i>
@@ -268,8 +139,7 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false"
-                            aria-controls="form-elements">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
                             <i class="menu-icon mdi mdi-card-text-outline"></i>
                             <span class="menu-title">Form elements</span>
                             <i class="menu-arrow"></i>
@@ -283,8 +153,7 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false"
-                            aria-controls="charts">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
                             <i class="menu-icon mdi mdi-chart-line"></i>
                             <span class="menu-title">Charts</span>
                             <i class="menu-arrow"></i>
@@ -298,8 +167,7 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#tables" aria-expanded="false"
-                            aria-controls="tables">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
                             <i class="menu-icon mdi mdi-table"></i>
                             <span class="menu-title">Tables</span>
                             <i class="menu-arrow"></i>
@@ -313,8 +181,7 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false"
-                            aria-controls="icons">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
                             <i class="menu-icon mdi mdi-layers-outline"></i>
                             <span class="menu-title">Icons</span>
                             <i class="menu-arrow"></i>
@@ -328,8 +195,7 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false"
-                            aria-controls="auth">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
                             <i class="menu-icon mdi mdi-account-circle-outline"></i>
                             <span class="menu-title">User Pages</span>
                             <i class="menu-arrow"></i>
@@ -370,33 +236,33 @@
             <!-- partial -->
             <div class="main-panel">
                 <?php
-        if (isset($_GET['page'])) {
-          $page = $_GET['page'];
-          if ($page == 'menu') {
-            include 'tabel_menu.php';
-          } else if ($page == 'menu_add') {
-            include 'menu_add.php';
-          } else if ($page == 'menu_delete') {
-            include 'menu_delete.php';
-          } else if ($page == 'menu_edit') {
-            include 'menu_edit.php';
-          } else if ($page == 'pesanan') {
-            include 'tabel_pesanan.php';
-          } else if ($page == 'pesanan_add') {
-            include 'pesanan_add.php';
-          } else if ($page == 'pesanan_delete') {
-            include 'pesanan_delete.php';
-          } else if ($page == 'pesanan_edit') {
-            include 'pesanan_edit.php';
-          } else if ($page == 'logout') {
-            include '../logout.php';
-          } else {
-            include 'main.php';
-          }
-        } else {
-          include 'main.php';
-        }
-        ?>
+                if (isset($_GET['page'])) {
+                    $page = $_GET['page'];
+                    if ($page == 'menu') {
+                        include 'tabel_menu.php';
+                    } else if ($page == 'menu_add') {
+                        include 'menu_add.php';
+                    } else if ($page == 'menu_delete') {
+                        include 'menu_delete.php';
+                    } else if ($page == 'menu_edit') {
+                        include 'menu_edit.php';
+                    } else if ($page == 'pesanan') {
+                        include 'tabel_pesanan.php';
+                    } else if ($page == 'pesanan_add') {
+                        include 'pesanan_add.php';
+                    } else if ($page == 'pesanan_delete') {
+                        include 'pesanan_delete.php';
+                    } else if ($page == 'pesanan_edit') {
+                        include 'pesanan_edit.php';
+                    } else if ($page == 'logout') {
+                        include '../logout.php';
+                    } else {
+                        include 'main.php';
+                    }
+                } else {
+                    include 'main.php';
+                }
+                ?>
             </div>
             <!-- content-wrapper ends -->
             <!-- partial:partials/_footer.html -->
