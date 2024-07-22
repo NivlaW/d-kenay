c6ba65cc9ca7fe142a6ab38d3339cb1da9343a21
NivlaW <1nivla1alvin@gmail.com>
2024-07-22 16:18:41 +0700

    update landing page

--- view/login.php
+++ view/login.php
@@ -28,90 +28,63 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>Star Admin2</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="../assets/vendors/feather/feather.css" />
-    <link
-      rel="stylesheet"
-      href="../assets/vendors/mdi/css/materialdesignicons.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="../assets/vendors/ti-icons/css/themify-icons.css"
-    />
-    <link
-      rel="stylesheet"
-      href="../assets/vendors/font-awesome/css/font-awesome.min.css"
-    />
+    <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css" />
+    <link rel="stylesheet" href="../assets/vendors/ti-icons/css/themify-icons.css" />
+    <link rel="stylesheet" href="../assets/vendors/font-awesome/css/font-awesome.min.css" />
     <link rel="stylesheet" href="../assets/vendors/typicons/typicons.css" />
-    <link
-      rel="stylesheet"
-      href="../assets/vendors/simple-line-icons/css/simple-line-icons.css"
-    />
-    <link
-      rel="stylesheet"
-      href="../assets/vendors/css/vendor.bundle.base.css"
-    />
-    <link
-      rel="stylesheet"
-      href="../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css"
-    />
+    <link rel="stylesheet" href="../assets/vendors/simple-line-icons/css/simple-line-icons.css" />
+    <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css" />
+    <link rel="stylesheet" href="../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css" />
     <!-- endinject -->
     <!-- Plugin css for this page -->
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <link rel="stylesheet" href="../assets/css/style.css" />
     <!-- endinject -->
-    <link rel="shortcut icon" href="../assets/images/favicon.png" />
-  </head>
-  <body>
+    <link rel="shortcut icon" href="../assets/images/icon2.png" />
+</head>
+
+<body>
     <div class="container-scroller">
-      <div class="container-fluid page-body-wrapper full-page-wrapper">
-        <div class="content-wrapper d-flex align-items-center auth px-0">
-          <div class="row w-100 mx-0">
-            <div class="col-lg-4 mx-auto">
-              <div class="auth-form-light text-left py-5 px-4 px-sm-5">
-                <div class="brand-logo">
-                  <img src="../assets/images/logo 1.svg" alt="logo" />
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth px-0">
+                <div class="row w-100 mx-0">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                            <div class="brand-logo">
+                                <img src="../assets/images/logo 1.svg" alt="logo" />
+                            </div>
+                            <h4>Login Admin</h4>
+                            <h6 class="fw-light">Login Dashboard.</h6>
+                            <form method="POST" class="pt-3">
+                                <div class="form-group">
+                                    <input type="text" name="username" class="form-control form-control-lg"
+                                        id="exampleInputEmail1" placeholder="Username" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" class="form-control form-control-lg"
+                                        id="exampleInputPassword1" placeholder="Password" />
+                                </div>
+                                <div class="mt-3 d-grid gap-2">
+                                    <input type="submit"
+                                        class="btn btn-block btn-primary btn-lg fw-medium auth-form-btn" name="submit"
+                                        class="btn btn-primary" value="login">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <h4>Login Admin</h4>
-                <h6 class="fw-light">Login Dashboard.</h6>
-                <form method="POST" class="pt-3">
-                  <div class="form-group">
-                    <input
-                      type="text"
-                      name="username"
-                      class="form-control form-control-lg"
-                      id="exampleInputEmail1"
-                      placeholder="Username"
-                    />
-                  </div>
-                  <div class="form-group">
-                    <input
-                      type="password"
-                      name="password"
-                      class="form-control form-control-lg"
-                      id="exampleInputPassword1"
-                      placeholder="Password"
-                    />
-                  </div>
-                  <div class="mt-3 d-grid gap-2">
-                    <input type="submit" class="btn btn-block btn-primary btn-lg fw-medium auth-form-btn" name="submit" class="btn btn-primary" value="login">
-                  </div>
-                </form>
-              </div>
             </div>
-          </div>
+            <!-- content-wrapper ends -->
         </div>
-        <!-- content-wrapper ends -->
-      </div>
-      <!-- page-body-wrapper ends -->
+        <!-- page-body-wrapper ends -->
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
@@ -127,5 +100,6 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     <script src="../assets/js/hoverable-collapse.js"></script>
     <script src="../assets/js/todolist.js"></script>
     <!-- endinject -->
-  </body>
+</body>
+
 </html>
