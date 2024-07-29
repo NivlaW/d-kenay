aede5e6113eb0fe18f5f607c866095ace431f0c4
rifqiaql <rifqiaqila@gmail.com>
2024-07-29 23:08:42 +0700

    fix multi_delete

--- view/tabel_pesanan.php
+++ view/tabel_pesanan.php
@@ -27,16 +27,6 @@
               </div>
             </div>
 
-<<<<<<< HEAD
-=======
-          <div class="d-flex gap-2">
-            <!-- <a href="index.php?page=pesanan_add" class="btn btn-outline-primary btn-fw"> <i class="fa fa-plus-square-o"></i> Tambah Data</a> -->
-            <form method="POST" action="index.php?page=multi_delete_pesanan">
-            <button type="submit" name="btn_delete" id="btn_delete" class="btn btn-danger" onclick="javascript: return confirm('apakah anda ingin menghapus data ini...?')"> <i class="fa fa-trash-o"></i> Hapus Data</button>
-            <a target="_blank" href="export_excel.php" class="btn btn-success"><i class="fa fa-file-o"></i> Export to Excel</a>
-            <a href="laporan_pesanan.php?awal=<?php echo isset($_POST['awal']) ? $_POST['awal'] : ''; ?>&akhir=<?php echo isset($_POST['akhir']) ? $_POST['akhir'] : ''; ?>" target="_blank" class="btn btn-info"><i class="fas fa-plus"></i> Cetak Data</a>
-          </div>
->>>>>>> fb0fc06ba2ce22b50575a59c67411061f57d619c
 
             <div class="d-flex gap-2">
               <!-- <a href="index.php?page=pesanan_add" class="btn btn-outline-primary btn-fw"> <i class="fa fa-plus-square-o"></i> Tambah Data</a> -->
